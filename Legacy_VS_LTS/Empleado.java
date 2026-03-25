17d92510d6af850eec517805d91a3c91fa61c7ed
SixOrca00-collab <sixorca00@gmail.com>
2026-03-25 08:30:32 -0500

    Termino investigacion sobre JVM y Java y su avance en el tiempo

--- Legacy_VS_LTS/Empleado.java
+++ Legacy_VS_LTS/Empleado.java
@@ -1,5 +1,40 @@
 package Legacy_VS_LTS;
 
 public class Empleado {
+    //Tipos de datos requeridos.
+    private byte nivel;
+    private short diasVacaciones;
+    private int edad;
+    private long salarioAnual; //Usar L al asignar
+    private float horasExtra; //Usar f al asignar
+    private double bono;
+    private char genero;
+    private Boolean activo;
+    //Tipo de objeto
+    private String nombreCompleto;
 
+    //Constructor
+    public Empleado(byte nivel, short diasVacaciones, int edad, long salarioAnual, float horasExtra, double bono, char genero, Boolean activo){
+        this.nivel = nivel;
+        this.diasVacaciones = diasVacaciones;
+        this.edad = edad;
+        this.salarioAnual = salarioAnual;
+        this.horasExtra = horasExtra;
+        this.bono = bono;
+        this.genero = genero;
+        this.activo = activo;
+        this.nombreCompleto = nombreCompleto;
+    }
+    // Getters
+        public int getEdad(){
+            return edad;
+        }
+        public String getnombreCompleto(){
+            return nombreCompleto;
+        }
+
+    //Setter 
+    public void setActivo(Boolean activo){
+        this.activo = activo; //Puede cambiar en cualquier momento
+    }
 }
--- NotasArquitectura.java
+++ NotasArquitectura.java
@@ -1,3 +1,31 @@
 public class NotasArquitectura {
+    public static void main(String[] args) { //TASK 1
+        /*Java 8 =  Se dice que Java 8 es el renacimiento de Java, incluso que es el nacimiento de "Java moderno".
+        Java deja el sistema tosco que tenia y pasa a ser mas funcional sin olvidar la POO, se establece un lenguaje mas "amigable" con el programador
+        fuera de eso antes Java era como tener que construir toda la maquina para un solo boton, un infierno para testeadores e incluso el mismo desarrollador,
+        Esto con ayuda de Lambdas()
+        Streams(),
+        Se corrigio el famoso NullPointerException, ya que antes de esto, ningun valor null podia modelarse.
+        Java 9-11= Sistema de modulos(Project Jigsaw)
+        En esta se definio que partes del codigo eran visibles, cuales no, control de dependencias pasa de ser un monton de codigo "abierto" a uno modular
+        encapsulado y organizado a gran escala solucionando conflictos entre librerias, dependencias ocultas y la JSHELL que permitio ejecutar el codigo
+        sin ejecutar clases completas.
+        inferencia de tipos(VAR) que permitia al compilador ayudarte, apoyarte de el sin eliminar el tipado fuerte, y optimizaba ya que no habia que escribir tanto
+        Luego en Java 11 trajeron LTS()
+        que fue una version a largo plazo que puso HTTP moderno, mejoras de rendimiento, limpieza de API'S antiguas. Aqui Java construyo los pilares para volverse
+        backend moderno.
+        De Java 12 a 17 se enfocaron a volver el codigo mas corto, claro y dificil de romper.
+        cambian los Switchs y añaden los textBlocks para fusionarse mejor con SQL, JSON y HTML.
+        Java 16 es de las mas importantes porque cambio datos de comportamiento, antes era muy repetitivo. Despues eso se generaba casi que automaticamente.
+        La paternidad paso a poder trabajar con conjuntos de manera mas agil y rapida.
+        SealedClases permiten tener mayor control sobre las herencias
+        Java 17 se enfoca en volverse una base solida con mejor desarrollo, rendimiento y lenguaje mas expresivo.
+        
+        Java 18-21
+        Se cambia el modo de Threads(hilos) por virtual Threads para ya que los hilos normales consumian espacio en memoria, eran toscos y pues podian variar segun el sistema 
+        operativo. Mientras que los virtuales eran mas ligero, lo otro es la Structure Concurrency: permitio organizar las tareas de modo que fueran mas faciles de seguir con 
+        un flujo mas claro y mayor control.
 
+        */
+    }
 }
