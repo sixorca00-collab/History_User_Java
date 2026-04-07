7fc9a57908312bc9aba69ab5a302f8d6e48dc7fc
SixOrca00-collab <sixorca00@gmail.com>
2026-04-07 08:05:34 -0500

    refactor(feat): add Tecnologias -> List and sedesDetalle -> Map

--- src/main/java/com/ModuleFive/Factory.java
+++ src/main/java/com/ModuleFive/Factory.java
@@ -1,9 +1,18 @@
 package main.java.com.ModuleFive;
 
-public record Factory(String nameCompany, byte sedes, String area) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public record Factory(String nameCompany, byte sedes, String area, List<String> tecnologias, Map<String, String> sedesDetalle) {
+
     public static void main(String[] args) {
-        Factory test = new Factory("RiwiCorp2026", (byte)3, "Desarrollo");
+        List<String> tech = List.of("Software","Hardware","IA");
+        Map<String, String> detalles = Map.of("Sede1:","Boyota","Sede2","Medellin");
+        Factory test = new Factory("RiwiCorp2026", (byte)3, "Desarrollo", tech, detalles);
         System.out.println(test);
     }
+
+
 }
 
