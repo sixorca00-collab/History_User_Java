991b88fc01f0339da8b2f392421846fa50a121e8
Sixorca <sixorca00@gmail.com>
2026-04-12 12:44:01 -0500

    docs(Persona): Add coment explain diference beetwen class and sealed class

--- src/main/java/com/ModuleFive/model/Persona.java
+++ src/main/java/com/ModuleFive/model/Persona.java
@@ -11,5 +11,11 @@ public abstract sealed class Persona permits EmpleadoClass, ConsultorExterno {
         return name;
     }
 
-
+    /* * Las Sealed Classes permiten manejar la herencia de modo seguro al restringir
+      qué clases pueden ser herederas mediante la palabra reservada 'permits'.
+      A diferencia de la herencia abierta, esto garantiza un sistema de tipos cerrado
+      que proporciona control total sobre el instanciamiento y la jerarquía de la API.
+      Esto permite que el compilador verifique que todos los casos posibles estén
+      cubiertos, evitando errores por tipos no previstos por el desarrollador.
+     */
 }
